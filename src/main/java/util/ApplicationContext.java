b33736481a1aae53cc074b9ca0240dbddc235bc4
Sepehr <sepehristaken@gmail.com>
2024-07-23 19:57:13 -0700

    update ApplicationContext.java

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -3,14 +3,27 @@ package util;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import repository.PersonRepository;
+import repository.StudentRepository;
+import repository.TeacherRepository;
+import repository.impl.PersonRepositoryImpl;
+import repository.impl.StudentRepositoryImpl;
+import repository.impl.TeacherRepositoryImpl;
 
 public class ApplicationContext {
     private static ApplicationContext instance;
     private EntityManagerFactory emf;
     private EntityManager em;
+    private PersonRepository personRepository;
+    private StudentRepository studentRepository;
+    private TeacherRepository teacherRepository;
 
     private ApplicationContext() {
-
+        emf = Persistence.createEntityManagerFactory("person");
+        em = emf.createEntityManager();
+        personRepository = new PersonRepositoryImpl(em, em.getTransaction());
+        studentRepository = new StudentRepositoryImpl(em, em.getTransaction());
+        teacherRepository = new TeacherRepositoryImpl(em, em.getTransaction());
     }
 
     public static ApplicationContext getInstance() {
